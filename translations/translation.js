70dff3aac78595b5f066ccfdc11b73a6615a023e
shppalushi <shppalushi@gmail.com>
2022-02-15 00:26:11 +0100

    Fixed the navbar issue

--- translations/translation.js
+++ translations/translation.js
@@ -1,11 +1,22 @@
 $(document).ready(() => {
-    var fr = fetch(`translations/en.json`)
-    .then(response => response.json())
-    .then(data =>{
-        for(var key in data){
-            $(`#${key}`).text(`${data[key]}`)
-        }
-    })
-    
-
+    loadTranslations(`translations/en.json`);
 })
+
+function changeLanguage(event) {
+    if ($(event.target).attr(`id`) == 'langEnglish') {
+        loadTranslations(`translations/en.json`);
+    }
+    else if ($(event.target).attr(`id`) == 'langSpanish') {
+        loadTranslations(`translations/Spanish.json`);
+    }
+}
+
+function loadTranslations(langFile) {
+    var fr = fetch(langFile)
+        .then(response => response.json())
+        .then(data => {
+            for (var key in data) {
+                $(`#${key}`).text(`${data[key]}`)
+            }
+        })
+}
